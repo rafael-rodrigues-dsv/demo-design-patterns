995df629830b5c173e4e2ec1a84f54557707457b
Rafael Rodrigues <rafael.rodrigues.dsv@gmail.com>
2023-06-10 22:45:47 -0300

    Implementa Flyweight

---
+++ structural/demo-flyweight/src/main/java/org/example/models/Tree/Tree.java
@@ -0,0 +1,19 @@
+package org.example.models.Tree;
+
+import java.awt.*;
+
+public class Tree {
+    private int x;
+    private int y;
+    private TreeType type;
+
+    public Tree(int x, int y, TreeType type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
+
+    public void draw(Graphics g) {
+        type.draw(g, x, y);
+    }
+}
