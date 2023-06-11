0ad20d41575e72803e555a945d2135ece8d7270a
Rafael Rodrigues <rafael.rodrigues.dsv@gmail.com>
2023-06-11 00:40:45 -0300

    Implementação do Composite Pattern

---
+++ structural/demo-composite/src/main/java/org/example/Main.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import org.example.models.Employee;
+
+/** Composite in Java.
+ * @author Rafael Rodrigues
+ * @reference https://www.tutorialspoint.com/design_pattern/composite_pattern.htm
+ * @version 1.0
+ * @since 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        testComposite();
+    }
+
+    private static void testComposite() {
+        Employee CEO = new Employee("John","CEO", 30000);
+
+        Employee headSales = new Employee("Robert","Head Sales", 20000);
+
+        Employee headMarketing = new Employee("Michel","Head Marketing", 20000);
+
+        Employee clerk1 = new Employee("Laura","Marketing", 10000);
+        Employee clerk2 = new Employee("Bob","Marketing", 10000);
+
+        Employee salesExecutive1 = new Employee("Richard","Sales", 10000);
+        Employee salesExecutive2 = new Employee("Rob","Sales", 10000);
+
+        CEO.add(headSales);
+        CEO.add(headMarketing);
+
+        headSales.add(salesExecutive1);
+        headSales.add(salesExecutive2);
+
+        headMarketing.add(clerk1);
+        headMarketing.add(clerk2);
+
+        //print all employees of the organization
+        System.out.println(CEO);
+
+        for (Employee headEmployee : CEO.getSubordinates()) {
+            System.out.println(headEmployee);
+
+            for (Employee employee : headEmployee.getSubordinates()) {
+                System.out.println(employee);
+            }
+        }
+    }
+}
